f4b85015b9c87acce71e290d4dcb698ec3216cc0
Mohammed Al Sakiti <mohammed110051@gmail.com>
2023-08-07 00:00:27 +0400

    Managing Invoice

---
+++ MainMenu.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace cSharp_Managing_Invoices
+{
+    internal class MainMenu
+    {
+        public void Menu()
+        {
+            // Menu Display
+            Console.WriteLine("Main Menu: \n1) Shop Settings.\n2) Manage Shop Items.\n3) Create New Invoice.\n4) Report Statistics.\n5)Report All Invoices.\n6) Search for Invoices.\n7) Program Statistics.\n8) Exit.");
+            while (true)
+            {
+                Console.Write("Please select an option: ");
+                string option = Console.ReadLine();
+                try
+                {
+                    // Check if the all inputs are numbers if not the program will loop 
+                    int NoOption = int.Parse(option);
+                    if (NoOption < 0 || NoOption > 9) { Console.WriteLine("Invalid input. Please enter a number between 1 and 8."); }
+                    switch (NoOption)
+                    {
+                        case 1:
+                            Console.WriteLine("1");
+                            break;
+                        case 2:
+                            Console.WriteLine("2");
+                            break;
+                        case 3:
+                            Console.WriteLine("3");
+                            break;
+                        case 4:
+                            Console.WriteLine("4");
+                            break;
+                        case 5:
+                            Console.WriteLine("5");
+                            break;
+                        case 6:
+                            Console.WriteLine("6");
+                            break;
+                        case 7:
+                            Console.WriteLine("7");
+                            break;
+                        case 8:
+                            Console.Write("Are you sure you want to exit? (y/n)"); // Check if the user want to exit the application
+                            string ExitInput = Console.ReadLine();
+                            if (ExitInput.Equals("y", StringComparison.OrdinalIgnoreCase))
+                            {
+                                Console.Write("Thank You");
+                                Environment.Exit(0);
+                            }
+                            else
+                            {
+                                Menu();
+                            } 
+                            break;
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid number.");
+                }
+            }
+        }    
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace cSharp_Managing_Invoices
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("      Welcome To    ");
+            Console.WriteLine(" +-+-+-+-+-+-+-+-+-+\r\n |W|h|a|t|I|s|F|u|n|\r\n +-+-+-+-+-+-+-+-+-+");
+            Console.WriteLine("  Managing Invoices  \n");
+
+            MainMenu mainMenu = new MainMenu();
+            mainMenu.Menu();
+        }
+    }
+}
