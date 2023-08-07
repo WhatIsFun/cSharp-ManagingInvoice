7d8d857acbb130238476d686f8f69c6500ec4258
WhatIsFun <mohammed110051@gmail.com>
2023-08-07 10:05:08 +0400

    Added a password to the program to secure the aplication

--- Program.cs
+++ Program.cs
@@ -4,12 +4,41 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("      Welcome To    ");
-            Console.WriteLine(" +-+-+-+-+-+-+-+-+-+\r\n |W|h|a|t|I|s|F|u|n|\r\n +-+-+-+-+-+-+-+-+-+");
-            Console.WriteLine("  Managing Invoices  \n");
 
-            MainMenu mainMenu = new MainMenu();
-            mainMenu.Menu();
+            string password = "112233";
+            int passTry = 5;
+            while (true)
+            {
+                Console.Write("Please enter the password>> ");
+                string pass = Console.ReadLine();
+                if (pass == password)
+                {
+                    Console.WriteLine("      Welcome To    ");
+                    Console.WriteLine(" +-+-+-+-+-+-+-+-+-+\r\n |W|h|a|t|I|s|F|u|n|\r\n +-+-+-+-+-+-+-+-+-+");
+                    Console.WriteLine("  Managing Invoices  \n");
+
+                    MainMenu mainMenu = new MainMenu();
+                    mainMenu.Menu();
+                }
+                else
+                {
+
+                    if (passTry == 0)
+                    {
+                        Console.WriteLine("All attempts are exhausted !!!");
+                        Environment.Exit(0);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Failed Password");
+                        Console.WriteLine($"You have {passTry} left. Be wise");
+                        passTry--;
+                    }
+                }
+            
+                
+            }
+            
         }
     }
 }
